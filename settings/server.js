0cce822baee1988eecbbc5db3666409f1411d221
h3gon101 <gabriel.rodrigues.0501@gmail.com>
2019-08-07 16:06:00 -0400

    WIP: Arrumando css.

--- settings/server.js
+++ settings/server.js
@@ -1,12 +1,14 @@
 const express = require ('express');
 const consign = require ('consign');
 const bodyParser = require('body-parser');
+const express_validator = require('express-validator')
 
 const app = express();
 app.set('view engine','ejs');
 app.set('views','./app/views');
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname + '/static'));
 
 consign()
     .include('app/routes')
