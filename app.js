70d73d554b2b965165f86e58329df05424da478f
h3gon101 <gabriel.rodrigues.0501@gmail.com>
2019-08-25 14:15:39 -0300

    Database(Mysql) successfuly connect with xampp.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const app = require('./settings/server');
 
+
+
 const port = 3000
 app.listen(port, function(){
 	console.log('Server running on localhost:' + port + '!');
--- settings/connection.js
+++ settings/connection.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 
-const mysql_connection = function(){
+const mysql_connection = () => {
 	console.log('Database is connected!');
 	return mysql.createConnection({
 			host : 'localhost',
@@ -11,8 +11,18 @@ const mysql_connection = function(){
 	});
 }
 
+mysql_connection().connect((err) => {
+	if(err){
+		console.log("Cannot Connect to Database. Error: " + err);
+	}
+	else{
+		console.log("Database connection successfully!");
+	}
+});
+
+
 module.exports = function(){
-	console.log('Running database connection!');
+	console.log('Running database connection..');
 	return mysql_connection;
 }
 		
