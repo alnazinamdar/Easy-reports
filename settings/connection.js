92909fb0bda636cdea8a0a4191e64e37719ea0b0
h3gon101 <gabriel.rodrigues.0501@gmail.com>
2019-08-30 15:26:45 -0300

    Conexão com DB4free criada.

--- settings/connection.js
+++ settings/connection.js
@@ -3,22 +3,21 @@ const mysql = require('mysql');
 const mysql_connection = () => {
 	console.log('Database is connected!');
 	return mysql.createConnection({
-			host : 'localhost',
-			port : 3306,
-			user : 'root',
-			password : '',
-			database : 'mysql_base'
+			host : 'db4free.net',
+			user : 'h3gon101',
+			password : 'H3gon100life!',
+			database : 'h3gon101'
 	});
 }
 
-// mysql_connection().connect((err) => {
-// 	if(err){
-// 		console.log("Cannot Connect to Database. Error: " + err);
-// 	}
-// 	else{
-// 		console.log("Database connection successfully!");
-// 	}
-// });
+mysql_connection().connect((err) => {
+	if(err){
+		console.log("Cannot Connect to Database. Error: " + err);
+	}
+	else{
+		console.log("Database connection successfully!");
+	}
+});
 
 
 module.exports = function(){
